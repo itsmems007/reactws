7cd54d8b9ce554dc2b64b4092106bf4507fb20bd
itsmems007 <saxena.mohit59@gmail.com>
2020-08-01 17:48:33 +0530

    Updates

---
+++ precache-manifest.7621de18b0f0d975f87f17bbb8169355.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a047b8ffc2192459b6a06fbe9afc783c",
+    "url": "/reactws/index.html"
+  },
+  {
+    "revision": "ebbac41fe6c767906dd3",
+    "url": "/reactws/static/css/2.0520b0af.chunk.css"
+  },
+  {
+    "revision": "9c4b0afbf97d7f1bbe34",
+    "url": "/reactws/static/css/main.16516185.chunk.css"
+  },
+  {
+    "revision": "ebbac41fe6c767906dd3",
+    "url": "/reactws/static/js/2.ad602c6a.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactws/static/js/2.ad602c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c4b0afbf97d7f1bbe34",
+    "url": "/reactws/static/js/main.e753503b.chunk.js"
+  },
+  {
+    "revision": "957bb26600a0f3926cf4",
+    "url": "/reactws/static/js/runtime-main.bf3724f1.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/reactws/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/reactws/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/reactws/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/reactws/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/reactws/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/reactws/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/reactws/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/reactws/static/media/software.43049cae.jpeg"
+  }
+]);
